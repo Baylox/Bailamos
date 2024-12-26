ba9319a4217c479e7a51cc15d417c664d7316689
Baylospawn <microsoftbaylo@laposte.net>
2024-12-26 03:55:28 +0100

    Add UserFixtures class for creating sample users and admin

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // Création d'un utilisateur
+        $user = new User();
+        $user->setEmail('Clarissia@BayAlamo.com');
+        $user->setRoles(['ROLE_TEACHER']);
+        $hashedPassword = $this->passwordHasher->hashPassword($user, 'password');
+        $user->setPassword($hashedPassword);
+
+        $manager->persist($user);
+
+        // Création d'un admin
+        $admin = new User();
+        $admin->setEmail('admin@BayAlamo.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $hashedPassword = $this->passwordHasher->hashPassword($admin, 'password');
+        $admin->setPassword($hashedPassword);
+
+        $manager->persist($admin);
+
+        // Enregistrer les données dans la base
+        $manager->flush();
+    }
+}
