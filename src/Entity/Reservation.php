5476b6300c81cf0e71dd76d7b0e36b9d6194e23c
Baylospawn <microsoftbaylo@laposte.net>
2024-12-29 05:28:07 +0100

    Add user-reservation relationship to User and Reservation entities

--- src/Entity/Reservation.php
+++ src/Entity/Reservation.php
@@ -13,8 +13,23 @@ class Reservation
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\ManyToOne(inversedBy: 'reservations')]
+    private ?User $user = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
     }
 }
