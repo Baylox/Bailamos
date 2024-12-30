379afd9de08dd925585ddb646f4174bdef1666c4
Baylospawn <microsoftbaylo@laposte.net>
2024-12-30 02:56:54 +0100

    Add Reservation entity and CRUD controller to admin dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,23 +3,24 @@
 namespace App\Controller\Admin;
 
 
+use App\Controller\Admin\UserCrudController;
 use App\Entity\Course;
+use App\Entity\DanceType;
+use App\Entity\Reservation;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\User;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
-use App\Controller\Admin\UserCrudController;
-use App\Entity\DanceType;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Core\User\UserInterface;
-use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -42,6 +43,9 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user-friends', User::class);
         yield MenuItem::linkToCrud('Les types de danse', 'fas fa-music', DanceType::class); 
         yield MenuItem::linkToCrud('Les différents cours', 'fas fa-person-running', Course::class);
+        yield MenuItem::linkToCrud('Les Réservations', 'fas fa-calendar-check', Reservation::class);
+
+
         yield MenuItem::linkToUrl('Accueil', 'fas fa-home', $this->generateUrl('app_home'));
         yield MenuItem::linkToLogout('Logout', 'fas fa-sign-out-alt');
     }
